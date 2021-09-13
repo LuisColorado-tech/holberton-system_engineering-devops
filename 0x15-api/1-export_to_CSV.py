e7002deecad120f1904257c1f83b14e359d34ac1
LuisColorado-tech <luiscolorado@outlook.com>
2021-09-13 18:41:37 -0500

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """script to export data in the CSV format"""
-  import requests
-  from sys import argv
-  import csv
+
 
 if __name__ == "__main__":
-
+    import requests
+    from sys import argv
+    import csv
     url = "https://jsonplaceholder.typicode.com/"
     user_id = argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
